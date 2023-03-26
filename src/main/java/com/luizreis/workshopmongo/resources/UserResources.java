cb489691b76e6479c869aa923e0a49848851f66c
Luiz Reis <reisluiz05@gmail.com>
2023-03-26 20:50:03 -0300

    User insert

--- src/main/java/com/luizreis/workshopmongo/resources/UserResources.java
+++ src/main/java/com/luizreis/workshopmongo/resources/UserResources.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping ;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,13 @@ public class UserResources {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<User> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
