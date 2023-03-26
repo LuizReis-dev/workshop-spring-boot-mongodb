f4e937ba65361da7198367ac3c91044758c56576
Luiz Reis <reisluiz05@gmail.com>
2023-03-25 21:33:16 -0300

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/luizreis/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.luizreis.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizreis.workshopmongo.domain.User;
+import com.luizreis.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
